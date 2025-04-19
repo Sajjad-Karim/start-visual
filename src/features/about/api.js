4386608b12f657824165c16397e13353a56fba4a
Asad Ali Asim <asadaliasim909@gmail.com>
2025-04-19 15:07:13 +0500

    feat: apis

--- src/features/about/api.js
+++ src/features/about/api.js
@@ -1,8 +1,8 @@
-import { STAR_VISUAL_API } from "../../http/config";
+import { STAR_VISUAL_API } from '../../http/config';
 
 export const saveAboutApi = (payload) =>
-  STAR_VISUAL_API.post("hero/upload", payload, {
-    headers: { "Content-Type": "multipart/form-data" },
+  STAR_VISUAL_API.post('about/addContent', payload, {
+    headers: { 'Content-Type': 'multipart/form-data' },
   });
 
-export const getAboutApi = () => STAR_VISUAL_API.get("hero/content");
+export const getAboutApi = () => STAR_VISUAL_API.get('about');
